8d000d39fd671d04b5fa17f01c5b1d7e35ac906e
raoxiaokang <raoxiaokang@hotmail.com>
2019-01-11 14:31:48 +0800

    main函数

--- src/source/App.java
+++ src/source/App.java
@@ -4,4 +4,7 @@ package source;
  * Created by Administrator on 2019/1/11/011.
  */
 public class App {
+    public static void main(String args[]){
+        System.out.println("hello,world");
+    }
 }
